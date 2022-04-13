1efaa80a92bac3f983cfdddbfceb08670f6eab9e
cecilio1877 <cecilio1877@disroot.org>
2022-04-13 00:29:18 -0500

    [create base project]

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export const App = () => {
+  return (
+    <View>
+      <Text>Medical Aplication Soon...</Text>
+    </View>
+  )
+}
